7c6c20c4685ef9044700c4d5130d35f522112e8c
Romero Dias <romero.dias@hotmail.com>
2017-06-07 17:15:21 -0300

    Format Code

--- jquery.progressbar.js
+++ jquery.progressbar.js
@@ -15,65 +15,60 @@
   */
 $.extend($.fn, {
 	pb: function(options) {
-			if(!options) options = {};
-			var me = this;
-			me.css({
-				color: "white", backgroundColor: "white", "width" : "1px"
+		if(!options) options = {};
+		var me = this;
+		me.css({
+			color: "white", backgroundColor: "white", "width" : "1px"
+		});
+		/* Default parameters */	
+		var defaultParameters = {
+			barColor: [{
+				"MIN": 0,  "MAX": 30, 	"BGCOLOR": "green",  "FONTCOLOR": "white" 
+			},{
+				"MIN": 31, "MAX": 60, 	"BGCOLOR": "yellow", "FONTCOLOR": "black" 
+			},{
+				"MIN": 60, "MAX": 100, "BGCOLOR": "red",    "FONTCOLOR": "white" 
+			}],
+			url: "pb.php",
+			interval: 2000
+		};
+		options = $.extend(defaultParameters, options);
+		/* Handle the bar background colors */
+		var defineColor = function(v, element) {
+			var color = {};
+			$.each(options.barColor, function(k, item){
+				if(v >= item.MIN && v <= item.MAX)
+					color = item;
 			});
-
-			/* Default parameters */	
-			var defaultParameters = {
-				barColor: [{
-					"MIN": 0,  "MAX": 30, 	"BGCOLOR": "green",  "FONTCOLOR": "white" 
-				},{
-					"MIN": 31, "MAX": 60, 	"BGCOLOR": "yellow", "FONTCOLOR": "black" 
-				},{
-					"MIN": 60, "MAX": 100, "BGCOLOR": "red",    "FONTCOLOR": "white" 
-				}],
-				url: "pb.php",
-				interval: 2000
-			};
-			options = $.extend(defaultParameters, options);
-		
-			/* Handle the bar background colors */
-			var defineColor = function(v, element) {
-				var color = {};
-				$.each(options.barColor, function(k, item){
-					if(v >= item.MIN && v <= item.MAX)
-						color = item;
-				});
-				element.css({
-					color : color.FONTCOLOR,
-					backgroundColor : color.BGCOLOR
-				});
-			}
-
-			/* Update the bar value */
-			var updateValue = function(data) {
-				if(data){
-					data = $.parseJSON(data);
-					var bar = me;
-					var time = data.SLA;
-					$(bar).html(time + "%");
-					$(bar).css({ "width" : time+"px"});
-					defineColor(time, bar);
-				}
-			}
-		
-			/* Search for a value on backend*/
-			var updateBar = function() {
-				$.ajax({
-					url: options.url,
-					method: 'post',
-					type: 'json',
-					success: updateValue
-				});
-			}
-			
-			/* Invoke the bar update function for a specific time interval */
-			var interval = setInterval(
-				function(){ updateBar(); },
-				options.interval
-			);		
+			element.css({
+				color : color.FONTCOLOR,
+				backgroundColor : color.BGCOLOR
+			});
 		}
-});
+		/* Update the bar value */
+		var updateValue = function(data) {
+			if(data){
+				data = $.parseJSON(data);
+				var bar = me;
+				var time = data.SLA;
+				$(bar).html(time + "%");
+				$(bar).css({ "width" : time+"px"});
+				defineColor(time, bar);
+			}
+		}
+		/* Search for a value on backend*/
+		var updateBar = function() {
+			$.ajax({
+				url: options.url,
+				method: 'post',
+				type: 'json',
+				success: updateValue
+			});
+		}
+		/* Invoke the bar update function for a specific time interval */
+		var interval = setInterval(
+			function(){ updateBar(); },
+			options.interval
+		);		
+	}
+);
